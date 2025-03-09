ebe57e8d5388b15d00c858076a2c8a561a303432
Guillermo Cabrera <guillermocabrera9710@hotmail.com>
2025-03-09 01:20:03 -0300

    add page

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,10 @@
 declare module '*.astro' {
-    import type { AstroComponentFactory } from 'astro/dist/runtime/server';
-    const Component: AstroComponentFactory;
-    export default Component;
-  }
+  import type { AstroComponentFactory } from 'astro/dist/runtime/server';
+  const Component: AstroComponentFactory;
+  export default Component;
+}
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
